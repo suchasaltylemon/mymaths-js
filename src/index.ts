8f0a2a40a1f3b6f804913f5ce921af4985798bd0
Sam <46686300+suchasaltylemon@users.noreply.github.com>
2022-02-04 23:01:32 +0000

    Added prompts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,94 @@
-import {
-  schoolPassword,
-  schoolUsername,
-  studentPassword,
-  studentUsername,
-} from "../auth.json";
 import { MyMaths } from "./core/mymaths";
+import inquirer from "inquirer";
+import $ from "assert";
 
-const mymaths = new MyMaths();
-
-const url = "https://app.mymaths.co.uk/362-homework/cumulative-frequency-1";
-
-mymaths
-  .login(schoolUsername, schoolPassword, studentUsername, studentPassword)
-  .then(async () => {
-    await mymaths.solveHomeworkFromUrl(url);
+const solveFromUrl = async (mymaths: MyMaths) => {
+  const ans = await inquirer.prompt({
+    message: "URL",
+    type: "input",
+    name: "url",
   });
+
+  const url = ans.url;
+  $(typeof url === "string");
+
+  await mymaths.solveHomeworkFromUrl(url);
+};
+
+const doAllHomework = (mymaths: MyMaths) => {
+  mymaths.getHomework().then((homework) => {
+    homework.forEach(async (hw) => {
+      await mymaths.solveHomework(hw);
+    });
+  });
+};
+
+const main = () => {
+  inquirer
+    .prompt([
+      {
+        message: "School Username",
+        type: "input",
+        name: "schoolUsername",
+      },
+      {
+        message: "School Password",
+        type: "password",
+        name: "schoolPassword",
+      },
+      {
+        message: "Student Username",
+        type: "input",
+        name: "studentUsername",
+      },
+      {
+        message: "Student Password",
+        type: "password",
+        name: "studentPassword",
+      },
+    ])
+    .then((answers) => {
+      const schoolUsername = answers.schoolUsername;
+      $(typeof schoolUsername === "string");
+
+      const schoolPassword = answers.schoolPassword;
+      $(typeof schoolPassword === "string");
+
+      const studentUsername = answers.studentUsername;
+      $(typeof studentUsername === "string");
+
+      const studentPassword = answers.studentPassword;
+      $(typeof studentPassword === "string");
+
+      const mymaths = new MyMaths();
+
+      mymaths
+        .login(schoolUsername, schoolPassword, studentUsername, studentPassword)
+        .then(() => {
+          inquirer
+            .prompt({
+              message: "What would you like to solve?",
+              type: "list",
+              name: "choice",
+              choices: [
+                {
+                  name: "Solve all homework",
+                  value: "all",
+                },
+                {
+                  name: "Solve from URL",
+                  value: "url",
+                },
+              ],
+            })
+            .then((answers) => {
+              const choice = answers.choice;
+              $(typeof choice === "string");
+
+              choice === "all" ? doAllHomework(mymaths) : solveFromUrl(mymaths);
+            });
+        });
+    });
+};
+
+main();
